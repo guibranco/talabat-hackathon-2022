896f7cb7b6eb185988be032565f811da8215c93a
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2022-07-05 16:50:44 +0400

    Add initial code and default response

---
+++ src/TalabatHackathon.API/Controllers/TranslateController.cs
@@ -0,0 +1,22 @@
+﻿namespace TalabatHackathon.API.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using TalabatHackathon.API.Models;
+
+    [ApiController]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiVersion("1.0")]
+    public class TranslateController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(typeof(TranslateResponseModel), 200)]
+        public IActionResult Translante([FromBody] TranslateRequestModel model, CancellationToken token)
+        {
+            return Ok(new TranslateResponseModel
+            {
+                Language = model.DestinationLanguage,
+                Text = model.Text
+            });
+        }
+    }
+}
---
+++ src/TalabatHackathon.API/Models/TranslateResponseModel.cs
@@ -0,0 +1,7 @@
+﻿namespace TalabatHackathon.API.Models;
+
+public class TranslateResponseModel
+{
+    public string Language { get; set; }
+    public string Text { get; set; }
+}
