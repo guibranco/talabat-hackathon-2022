82ee39541045a82a9978da909f2d9f2fbaa74e09
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2022-07-05 18:06:52 +0400

    Fix building

--- src/TalabatHackathon.API/Controllers/SpeechController.cs
+++ src/TalabatHackathon.API/Controllers/SpeechController.cs
@@ -1,5 +1,4 @@
-﻿using System.Net;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using TalabatHackathon.API.Models;
 
 namespace TalabatHackathon.API.Controllers
--- src/TalabatHackathon.API/Program.cs
+++ src/TalabatHackathon.API/Program.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Versioning;
 
 var builder = WebApplication.CreateBuilder(args);
 
