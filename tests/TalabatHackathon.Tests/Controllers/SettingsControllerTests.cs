d38a4e8b47f6fd45a182e353109584ca91759c70
Guilherme Branco Stracini <guilhermes@outsurance.ie>
2024-05-19 06:17:09 +0100

    Fix test suffix

---
+++ tests/TalabatHackathon.Tests/Controllers/SettingsControllerTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using FluentAssertions;
+using NSubstitute;
+using TalabatHackathon.API.Controllers;
+using Xunit;
+
+namespace TalabatHackathon.API.Controllers
+{
+    public class SettingsControllerTests
+    {
+        public SettingsControllerTests() { }
+
+        private SettingsController CreateSettingsController()
+        {
+            return new SettingsController();
+        }
+
+        [Fact]
+        public void GetSettings_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var settingsController = this.CreateSettingsController();
+
+            // Act
+            var result = settingsController.GetSettings();
+
+            // Assert
+            result.Should().NotBeNull();
+        }
+    }
+}
